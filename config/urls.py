d3995b878af7ac5e54f4787c14442708948b9f7d
khengleng <khenglengt@gmail.com>
2026-03-27 18:44:29 +0700

    Fix Railway healthcheck and run migrations on startup

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("healthz", lambda request: JsonResponse({"ok": True}), name="healthz"),
     path("", include("chat.urls")),
 ]
