e106f5940391c43fcd58d3abc7ab294da91b4882
khengleng <khenglengt@gmail.com>
2026-03-28 16:16:51 +0700

    Rename Django admin header to Fortune Telling Administration

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
 
+admin.site.site_header = "Fortune Telling Administration"
+admin.site.site_title = "Fortune Telling Administration"
+admin.site.index_title = "Fortune Telling Administration"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("healthz", lambda request: JsonResponse({"ok": True}), name="healthz"),
